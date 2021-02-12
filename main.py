f19348a4135e8bbc97d73f0ccdb6adf0c9e3703c
Dyskal <yoshilexis@gmail.com>
2021-02-12 11:36:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-import os
+from os import environ
 
 import discord
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='&')
-token = os.environ.get('TOKEN')
+token = environ.get('TOKEN')
+
 
 @client.event
 async def on_ready():
     print('Soldat Mécanique en Ligne.')
 
+
 @client.command()
 async def ban(ctx, user: discord.User, *, reason="Aucune raison n'a été spécifiée."):
-    await ctx.guild.ban(user, reason = reason)
+    await ctx.guild.ban(user, reason=reason)
     embed = discord.Embed(title="**Bannissement**", description="Ce membre a été banni du serveur",
                           url="https://www.discord.gg/a7KHDT9gFT", colour=discord.Colour.red())
     embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/796861724366995457.gif")
@@ -22,9 +24,10 @@ async def ban(ctx, user: discord.User, *, reason="Aucune raison n'a été spéci
     embed.set_footer(text="discord.gg/a7KHDT9gFT")
     await ctx.send(embed=embed)
 
+
 @client.command()
 async def kick(ctx, user: discord.User, *, reason="Aucune raison n'a été spécifiée."):
-    await ctx.guild.kick(user, reason = reason)
+    await ctx.guild.kick(user, reason=reason)
     embed = discord.Embed(title="**Expulsion**", description="Ce membre a été expulsé du serveur",
                           url="https://www.discord.gg/a7KHDT9gFT", colour=discord.Colour.orange())
     embed.set_thumbnail(url="https://media.tenor.com/images/27f16871c55a3376fa4bfdd76ac2ab5c/tenor.gif")
@@ -34,13 +37,13 @@ async def kick(ctx, user: discord.User, *, reason="Aucune raison n'a été spéc
     embed.set_footer(text="discord.gg/a7KHDT9gFT")
     await ctx.send(embed=embed)
 
+
 @client.command(name='del')
 async def delete(ctx, number_of_messages: int):
     messages = await ctx.channel.history(limit=number_of_messages + 1).flatten()
 
     for each_messages in messages:
         await each_messages.delete()
-
 
 
 client.run(token)
