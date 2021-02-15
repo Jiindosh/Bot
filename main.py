4e314c7861c0d779d9840f99caeb5391260eea94
Jiindosh <77612147+Jiindosh@users.noreply.github.com>
2021-02-15 19:18:53 +0100

    Fix code

--- main.py
+++ main.py
@@ -45,26 +45,5 @@ async def delete(ctx, number_of_messages: int):
     for each_messages in messages:
         await each_messages.delete()
 
-@client.command()
-async def userinfo(ctx, *, user: discord.Member = None):
-    if user is None:
-        user = ctx.author
-    date_format = "%a, %d %b %Y %I:%M %p"
-    embed = discord.Embed(color=0xdfa3ff, description=user.mention)
-    embed.set_author(name=str(user), icon_url=user.avatar_url)
-    embed.set_thumbnail(url=user.avatar_url)
-    embed.add_field(name="Joined", value=user.joined_at.strftime(date_format))
-    members = sorted(ctx.guild.members, key=lambda m: m.joined_at)
-    embed.add_field(name="Join position", value=str(members.index(user)+1))
-    embed.add_field(name="Registered", value=user.created_at.strftime(date_format))
-    if len(user.roles) > 1:
-        role_string = ' '.join([r.mention for r in user.roles][1:])
-        embed.add_field(name="Roles [{}]".format(len(user.roles)-1), value=role_string, inline=False)
-    perm_string = ', '.join([str(p[0]).replace("_", " ").title() for p in user.guild_permissions if p[1]])
-    embed.add_field(name="Guild permissions", value=perm_string, inline=False)
-    embed.set_footer(text='ID: ' + str(user.id))
-    return await ctx.send(embed=embed)
-    if isinstance(ctx.channel, discord.DMChannel):
-        return
 
 client.run(token)
